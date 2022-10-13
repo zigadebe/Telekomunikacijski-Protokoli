f64c97c85f63aeed0fd2584c75cf7fc4975c6a01
zigadebe <83421755+zigadebe@users.noreply.github.com>
2022-10-13 19:27:13 +0200

    dodana selujoca domaca naloga 2

---
+++ DN_01/domaca_naloga_02.py
@@ -0,0 +1,11 @@
+# Napišite program seštevalnik. 
+# Program naj od uporabnika zahteva dve števili. 
+# Ti dve števili naj sešteje in vrednost izpiše.
+
+print("------------------ PROGRAM SESTEVALNIK ------------------")
+print("-")
+st1 = float(input("- Vnesite prvo stevilo: "))
+st2 = float(input("- Vnesite drugo stevilo: "))
+print("-")
+print("- Vasi stevili sem sestel in dobil stevilo " + str(st1 + st2))
+print("---------------------------------------------------------")
