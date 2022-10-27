af917dff762c8df38f2a112df77a8952dc40b7c3
zigadebe <83421755+zigadebe@users.noreply.github.com>
2022-10-27 20:51:06 +0200

    resena naloga 1

---
+++ DN_02/DN02_naloga_1.py
@@ -0,0 +1,9 @@
+# ----------- NALOGA 1 -----------
+# Iz sledečega list-a pridobite vrednost ffff in jo shranite v spremenljivko f_vrednost: our_list = ["a", ["bb", "cc"], "d", [["eee"], ["ffff"], "ggg"]]
+
+our_list = ["a", ["bb", "cc"], "d", [["eee"], ["ffff"], "ggg"]]
+
+f_vrednost = our_list[3][1]
+print(f_vrednost)
+
+# -------------------------------
