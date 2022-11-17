b4c2846f3f51264481bb5a128a5c774ffe15badf
zigadebe~ <zigide@gmail.com>
2022-11-17 10:30:20 +0100

    koncana delujoca "DN_03 naloga 1"

--- DN_03/DN_03_naloga_1.py
+++ DN_03/DN_03_naloga_1.py
@@ -3,23 +3,36 @@
 # Funkcija naj nato izpiše koliko let je uporabnik star (upoštevamo samo letnico rojstva) in vrne število let (celoštevilska vrednost). 
 # EMŠO ima 14 številk XXXXyyyXXXXXXX. 5.,6.,7. številka predstavljajo letnico rojstva (999 -> 1999 leto rojstva).
 
-
+from datetime import date
 
 def emso_leta_preracun():
     emso = input('Vnesi svoj EMSO: ')
     leto = ''
+    tisocletje = 2000
 
     for i in range(4,7):
         leto = leto + emso[i]
     
+    if (int(emso[4]) == 9):
+        tisocletje = 1000
+    elif (int(emso[4]) == 9):
+        tisocletje = 1000
+    
+    print(emso[4])
+    print(tisocletje)
+    
     leto = int(leto)
+    letorojstva = leto + tisocletje
 
-    leto = leto + 2000
+    # Textual month, day and year
+    today = date.today()	
+    trenutnoLeto = int(today.strftime("%Y"))
 
-    print(str(leto))
+
+    starost = trenutnoLeto - letorojstva
+    return starost
 
 # primer uporabe 1
-#starost = emso_leta_preracun()
-
-emso_leta_preracun()
+starost = emso_leta_preracun()
+print("Vaša starost je " + str(starost) + " let.")
 # -------------------------------
