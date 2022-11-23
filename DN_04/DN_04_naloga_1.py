285528c49c5b691a5b1719c756efede94cb02d5b
zigadebe~ <zigide@gmail.com>
2022-11-23 11:22:52 +0100

    resena naloga DN_04_naloga1

--- DN_04/DN_04_naloga_1.py
+++ DN_04/DN_04_naloga_1.py
@@ -1,8 +1,14 @@
 # ----------- NALOGA 1 -----------
 #Z pomočjo knjižnice json pretvorite spodnji slovar podatki v JSON besedilo.
 
+import json
+
 podatki = {
   "name": "John",
   "age": 30,
   "city": "New York"
 }
+
+besedilo = json.dumps(podatki)
+
+print(besedilo)
