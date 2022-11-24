4fdb7d6398987974ea3943f6840eb9f471a5c2ba
zigadebe <83421755+zigadebe@users.noreply.github.com>
2022-11-24 18:50:38 +0100

    koncana naloga DN_04_naloga_3

--- DN_04/DN_04_naloga_3.py
+++ DN_04/DN_04_naloga_3.py
@@ -2,3 +2,14 @@
 # Namestite knjižnico requests in spišite funkcijo, ki za iskalni niz vrne html kodo rtvslo 
 # iskalnika novic (https://www.rtvslo.si/iskalnik?q=iskalni niz).
 
+import requests
+
+def pridobiHTML (povezava):
+    iskanje = 'https://www.rtvslo.si/iskalnik?q=' + str(povezava)
+    r = requests.get(iskanje)
+    return r
+
+iskalniNiz = input('vnesi iskalni niz: ')
+
+print(pridobiHTML(iskalniNiz))
+
